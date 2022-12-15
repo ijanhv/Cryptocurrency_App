e174663a5b8b0c2c0c5f946cdfc9e92934f7da6d
Janhavi <patil.janhavi1207@gmail.com>
2022-12-15 13:20:54 +0530

    added created smart contract

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -29,8 +29,8 @@ const handleSubmit = () => {
 
   return (
     <div className="flex w-full justify-center">
-      <div className="flex md:flex-row flex-col items-start md:p-20 py-12 px-4">
-        <div className="flex flex-1 justify-start flex-col md:mr-10">
+      <div className="flex mf:flex-row flex-col items-start md:p-20 py-12 px-4">
+        <div className="flex flex-1 justify-start flex-col mf:mr-10">
           <h1 className="tex-3xl sm:text-5xl text-white text-gradient py-1">
             Send Crypto <br /> across the world
           </h1>
@@ -64,7 +64,7 @@ const handleSubmit = () => {
           </div>
         </div>
 
-        <div className="flex flex-col flex-1 items-center justify-start w-full md:mt-0 mt-10">
+        <div className="flex flex-col flex-1 items-center justify-start w-full mf:mt-0 mt-10">
           <div className="p-3 justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card white-glassmorphism">
             <div className="flex justify-between flex-col w-full h-full">
               <div className="flex justify-between items-start">
@@ -91,7 +91,7 @@ const handleSubmit = () => {
                 <Input placeholder="Enter message" name="message" type="text" handleChange={() => {}}  />
 
                 <div className="h-[1px] w-full bg-gray-400 my-2" />
-                    {true ? (
+                    {!true ? (
                         <Loader />
                     ) : (
                         <button
